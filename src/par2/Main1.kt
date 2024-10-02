6df9dc001d481ed860c055af22a283d3e96110f5
DebtOfTime <sasha_kulagin098311@mail.ru>
2024-10-02 16:18:55 +0300

    1

---
+++ src/par2/Main1.kt
@@ -0,0 +1,13 @@
+package par2
+
+import java.util.concurrent.ArrayBlockingQueue
+
+fun main() {
+    val x = readln().toInt()
+    val y = readln().toInt()
+    val array = arrayOfNulls<Number>(x)
+    for(i in 0..x)
+    {
+
+    }
+}
