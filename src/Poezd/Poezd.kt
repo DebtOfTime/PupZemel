5e94a7012c295bcd823f95293d2a87b65e59f2e3
DebtOfTime <135342593+DebtOfTime@users.noreply.github.com>
2024-12-11 14:03:15 +0300

    PoezdFix

--- src/Poezd/Poezd.kt
+++ src/Poezd/Poezd.kt
@@ -1,6 +1,10 @@
 package Poezd
 
+import kotlin.random.Random
+
 fun main() {
+    val Passangers = Random.nextInt(5,202)
     val cities = listOf("Москва", "Санкт-Петербург", "Казань", "Екатеринбург", "Новосибирск")
-    val train = SamPoezd(passengers = 63, cities)
+    val train = SamPoezd(Passangers, cities)
+    println("Всего пассажиров: ${Passangers}")
 }
--- src/Poezd/SamPoezd.kt
+++ src/Poezd/SamPoezd.kt
@@ -9,16 +9,12 @@ class SamPoezd(
     private val wagons = mutableListOf<Wagon>()
 
     init {
-        // Генерация маршрута
         val (city1, city2) = selectCities()
         println("Маршрут поезда: $city1 -> $city2")
-
-        // Расчет необходимых вагонов
         calculateWagons()
     }
 
     private fun selectCities(): Pair<String, String> {
-        // Выбираем два уникальных города на случайный
         val city1 = cities.random()
         val city2 = cities.filter { it != city1 }.random()
         return Pair(city1, city2)
@@ -27,15 +23,13 @@ class SamPoezd(
     private fun calculateWagons() {
         var remainingPassengers = passengers
         while (remainingPassengers > 0) {
-            val capacity = Random.nextInt(5, 26) // Рандомная вместимость от 5 до 25 мест
+            val capacity = Random.nextInt(5, 26)
             val wagon = Wagon(capacity)
             wagons.add(wagon)
             remainingPassengers -= capacity
             println("Добавлен вагон на $capacity мест(-а)")
         }
         println("Общее количество вагонов: ${wagons.size}")
-
-        // Подсчет оставшихся свободных мест
         val totalCapacity = wagons.sumOf { it.capacity }
         val freeSeats = totalCapacity - passengers
         println("Количество свободных мест: $freeSeats")
